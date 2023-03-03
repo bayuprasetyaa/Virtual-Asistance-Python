bd8154cf500b9df3bc428d841e09875badac6e02
Bayu Prasetya <bayud40@gmail.com>
2023-03-03 13:14:42 +0700

    features-bot

--- Assistant.py
+++ Assistant.py
@@ -1,32 +1,39 @@
-# Case Study: Virtual Assistant using Python (Assistant.py)
 
-# CLUE: import necessary libraries here
+import os
+import json
+import datetime
 
-class Bot:
+from bot import Bot
 
-    def __init__(self, name):
-        
-        self.__name = name
+class Assistant(Bot):
     
-    def set_bot(self, name:str) -> None:
-        """Set Bot Name
-        Args:
-            name (str)
-        """
-        pass 
-    
-    def get_bot(self) -> str:
-        """Get Bot Name
-        Returns:
-            str
-        """
-        pass
-
-class Assistance(Bot):
+    __PATH = os.getcwd()
+    __BOT = 'bot.json'
+    __SCHEDULE = 'schedule.txt'
     
     def __init__(self):
         super().__init__()
+        self.initialize()
     
+    def run(self):
+        pass
+    
+    def initialize(self):
+        
+        print("Initialize ...")
+        for file in os.listdir(self.__PATH):
+            if file == self.__BOT:
+                super().bot_init(path=os.path.join(self.__PATH, self.__BOT))
+                print("Initialize completed!")
+                print(f"Hello {self.get_user()}, {self.get_bot_name()} here. How can I help ?")
+                break
+        else:
+            print(f"Hi, this is the first time we meet. My name Jarvis")
+            print("What is your name? ")
+            user = input("Your name: ")
+            self.bot_init(path=self.__BOT, user=user)
+            print(f"Hello {self.get_user()}, {self.get_bot_name()} here. How can I help ?")
+        
     def create_schedule(self):
         pass
     
@@ -39,3 +46,12 @@ class Assistance(Bot):
     def send_email(self):
         pass
     
+    
+    def bot_init(self, path, user, name='Jarvis'):
+        
+        with open(path, 'w') as bot:
+            self.set_bot(name)
+            self.set_born(str(datetime.datetime.now()))
+            self.set_user(user)
+            json.dump(super().__dict__, bot)
+
